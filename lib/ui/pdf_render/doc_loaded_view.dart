bc65f9c36a83d556ba9fb4d103c3c7a0ac823c4f
enix403 <fog.code000@gmail.com>
2023-10-31 22:23:08 +0500

    Render pdf

--- lib/ui/pdf_render/doc_loaded_view.dart
+++ lib/ui/pdf_render/doc_loaded_view.dart
@@ -17,7 +17,7 @@ class PdfDocLoadedView extends StatelessWidget {
       builder: (context, constraints) {
         final size = Size(constraints.maxWidth, constraints.maxHeight);
 
-        return MeasuredCanvas(canvasSize: size);
+        return MeasuredCanvas(canvasSize: size, pipeline: pipeline);
       },
     );
   }
@@ -25,10 +25,12 @@ class PdfDocLoadedView extends StatelessWidget {
 
 class MeasuredCanvas extends StatefulWidget {
   final Size canvasSize;
+  final RenderPipeline pipeline;
 
   const MeasuredCanvas({
     super.key,
     required this.canvasSize,
+    required this.pipeline,
   });
 
   @override
@@ -36,76 +38,53 @@ class MeasuredCanvas extends StatefulWidget {
 }
 
 class _MeasuredCanvasState extends State<MeasuredCanvas> {
-  bool zooming = false;
-  double _baseScaleFactor = 1.0;
-  double _currentScaleFactor = 1.0;
-
-  Offset focalPoint = Offset.zero;
-
   double get canvasWidth => widget.canvasSize.width;
   double get canvasHeight => widget.canvasSize.height;
 
-  double get windowWidth => widget.canvasSize.width / _currentScaleFactor;
-  double get windowHeight => widget.canvasSize.height / _currentScaleFactor;
+  RenderPipeline get pipeline => widget.pipeline;
+
+  int _colorIndex = 0;
 
   @override
   void initState() {
     super.initState();
-
-    focalPoint = Offset(canvasWidth, canvasHeight) / 2;
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: (details) {
-        _baseScaleFactor = _currentScaleFactor;
-        zooming = true;
-      },
-      onScaleUpdate: (details) {
-        if (details.scale == 1) {
-          setState(() {
-            focalPoint = focalPoint.translate(details.focalPointDelta.dx, 0);
-          });
-          return;
-        }
+    return ListView.builder(
+      itemBuilder: (_, index) {
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        print("Build index $index");
+        if (index >= pipeline.totalPages) return null;
 
-        setState(() {
-          focalPoint = details.localFocalPoint;
-          _currentScaleFactor =
-              (_baseScaleFactor * details.scale).clamp(1.0, 3.0);
-        });
-      },
-      onScaleEnd: (_) {
-        zooming = false;
-      },
-      child: Stack(
-        children: [
-          Container(
-            width: canvasWidth,
-            height: canvasHeight,
-            color: Colors.deepPurple,
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment(
-                (focalPoint.dx / canvasWidth) * 2 - 1,
-                (focalPoint.dy / canvasHeight) * 2 - 1,
-              ),
+        final logicalSize = pipeline.logicalSizes.sizeAt(index);
+        final physicalWidth =
+            canvasWidth * logicalSize.width / pipeline.maxLpWidth;
+        final physicalHeight = physicalWidth / logicalSize.aspectRatio;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _colorIndex = (_colorIndex + 1) % 3;
+                });
+              },
               child: Container(
-                width: windowWidth,
-                height: windowHeight,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2.0,
-                    color: Colors.green[300]!,
-                  ),
-                ),
+                color: [Colors.purple, Colors.green, Colors.red][_colorIndex],
+                width: physicalWidth,
+                height: physicalHeight,
               ),
             ),
-          ),
-        ],
-      ),
+            const SizedBox(
+              height: 8,
+            )
+          ],
+        );
+      },
     );
   }
 }
